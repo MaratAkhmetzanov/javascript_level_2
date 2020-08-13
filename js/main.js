4c8977d6334a8439a8351c2d1c0c2184824d6b2b
mario <mario@BASE.localdomain>
2020-08-14 00:32:43 +0500

    small fix

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ const products = [
 
 /**
  * Функция для формирования вёрстки каждого товара.
- * @param {obj} product объект товара;
+ * @param {string} title название товара;
+ * @param {number} price цена товара.
  */
-const renderProduct = (product = { title: 'Без названия', price: 'n/a' }) => {
+const renderProduct = (title = 'Без названия', price = 'n/a') => {
   return `<div class="product-item">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
+                <h3>${title}</h3>
+                <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`;
 };
@@ -24,8 +25,9 @@ const renderProduct = (product = { title: 'Без названия', price: 'n/a
  * @param {array} products список товаров, которые нужно добавить на страницу.
  */
 const renderPage = (products) => {
-  const productsList = products.map((item) => renderProduct(item));
-  document.querySelector('.products').innerHTML = productsList.join('');
+  document.querySelector('.products').innerHTML = products
+    .map((item) => renderProduct(item.title, item.price))
+    .join('');
 };
 
 renderPage(products);

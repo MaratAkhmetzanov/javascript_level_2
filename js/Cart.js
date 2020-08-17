55b4da77236b842b36dfa57dd2572a07176e1407
mario <mario@BASE.localdomain>
2020-08-18 00:52:34 +0500

    Lesson 2. (1,2)

---
+++ js/Cart.js
@@ -0,0 +1,83 @@
+class Cart {
+  constructor(container = '.products') {
+    this.container = container;
+    this.cartList = [];
+    this.totalPrice = this._totalPriceCalc();
+  }
+
+  /**
+   * Добавляет продукт в корзину.
+   * @param {Object} product объект продукта
+   */
+  addToCart(product) {
+    this.cartList.push(product);
+  }
+
+  /**
+   * Удаляет продукт из корзины
+   * @param {int} id идентификатор продукта
+   */
+  removeFromCart(id) {
+    this.cartList.splice(id, 1);
+    this.render();
+  }
+
+  /**
+   * Очищает корзину
+   */
+  clearCart() {
+    this.cartList = [];
+    this.render();
+  }
+
+  /**
+   * Высчитывает общую стоимость корзины и записывает в свойство корзины
+   */
+  _totalPriceCalc() {
+    this.totalPrice = 0;
+    this.cartList.forEach((item) => {
+      this.totalPrice += item.price;
+    });
+  }
+
+  /**
+   * Отрисовывает корзину и добавляет событие нажатия на кнопку удаления
+   */
+  render() {
+    const block = document.querySelector(this.container);
+    block.innerHTML = '';
+    for (let i = 0; i < this.cartList.length; i++) {
+      const item = new CartItem(this.cartList[i], i);
+      block.insertAdjacentHTML('beforeend', item.render());
+    }
+    document.querySelectorAll('.remove-btn').forEach((btn) => {
+      btn.addEventListener('click', () => {
+        this.removeFromCart(btn.dataset.id);
+      });
+    });
+  }
+}
+
+class CartItem {
+  constructor(product, id, img = 'https://placehold.it/200x150') {
+    this.img = img;
+    this.id = product.id;
+    this.title = product.title;
+    this.price = product.price;
+    this.itemId = id;
+  }
+
+  /**
+   * Возвращает разметку для продукта в корзине
+   */
+  render() {
+    return `<div class="cart-item">
+				<img src="${this.img}">
+				<h3>${this.title}</h3>
+				<p>${this.price}</p>
+				<button class="remove-btn" data-id="${this.itemId}">Удалить</button>
+			</div>`;
+  }
+}
+
+let cart = new Cart();
--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
-const products = [
-  { id: 1, title: 'Notebook', price: 2000 },
-  { id: 2, title: 'Mouse', price: 20 },
-  { id: 3, title: 'Keyboard', price: 200 },
-  { id: 4, title: 'Gamepad', price: 50 },
-  { id: 5, price: 50 },
-  { id: 6 },
-];
+class ProductList {
+  constructor(container = '.products') {
+    this.container = container;
+    this.goods = [];
+    this._fetchProducts();
+    this.render();
+  }
+  _fetchProducts() {
+    this.goods = [
+      { id: 1, title: 'Notebook', price: 2000 },
+      { id: 2, title: 'Mouse', price: 20 },
+      { id: 3, title: 'Keyboard', price: 200 },
+      { id: 4, title: 'Gamepad', price: 50 },
+    ];
+  }
 
-/**
- * Функция для формирования вёрстки каждого товара.
- * @param {string} title название товара;
- * @param {number} price цена товара.
- */
-const renderProduct = (title = 'Без названия', price = 'n/a') => {
-  return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`;
-};
+  render() {
+    const block = document.querySelector(this.container);
+    for (let product of this.goods) {
+      const item = new ProductItem(product);
+      block.insertAdjacentHTML('beforeend', item.render());
+    }
+  }
+}
 
-/**
- * Для каждого элемента массива, запускает функцию renderProduct.
- * @param {array} products список товаров, которые нужно добавить на страницу.
- */
-const renderPage = (products) => {
-  document.querySelector('.products').innerHTML = products
-    .map((item) => renderProduct(item.title, item.price))
-    .join('');
-};
+class ProductItem {
+  constructor(product, img = 'https://placehold.it/200x150') {
+    this.title = product.title;
+    this.id = product.id;
+    this.price = product.price;
+    this.img = img;
+  }
+  render() {
+    return `<div class="product-item">
+              <img src="${this.img}">
+              <h3>${this.title}</h3>
+              <p>${this.price}</p>
+              <button class="buy-btn">Купить</button>
+          </div>`;
+  }
+}
 
-renderPage(products);
+let list = new ProductList();

8a104f72555259ce632f5bc58ac4bf7fe1d4a4dd
mario <mario@BASE.localdomain>
2020-09-05 14:10:00 +0500

    stats file

--- server/handler.js
+++ server/handler.js
@@ -1,10 +1,30 @@
 const cart = require('./cart');
 const fs = require('fs');
+const moment = require('moment');
 
 const actions = {
   add: cart.add,
   change: cart.change,
   remove: cart.remove
+};
+
+let writeStats = (action, product) => {
+  fs.readFile('server/db/stats.json', 'utf-8', (err, data) => {
+    let newStats = JSON.parse(data);
+
+    let statsObj = {
+      time: moment(),
+      action: action,
+      productId: product.params.id
+    };
+
+    newStats.stats.push(statsObj);
+    fs.writeFile(
+      'server/db/stats.json',
+      JSON.stringify(newStats, null, 4),
+      (err) => {}
+    );
+  });
 };
 
 //HANDLER отвечает за изменение данных в самом файле
@@ -18,6 +38,7 @@ let handler = (req, res, action, file) => {
         if (err) {
           res.sendStatus(404, JSON.stringify({ result: 0, text: err }));
         } else {
+          writeStats(action, req);
           res.send(JSON.stringify({ result: 1 }));
         }
       });
